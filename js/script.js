f80520d46e47ec25328a301113632f7503ccd6bb
Jakub Broton <jakub.broton@gmail.com>
2023-04-06 18:24:17 +0200

    Add computer moves

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumer == 2) {
+    computerMove = 'papier';
+} else if (randomNumer == 3) {
+    computerMove = 'nozyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
