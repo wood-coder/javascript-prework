4ed49788e66ad23fada147df9b7802e6085fa661
Jakub Broton <jakub.broton@gmail.com>
2023-04-14 09:58:12 +0200

    Remove comments from script.js

--- js/script.js
+++ js/script.js
@@ -6,33 +6,11 @@ function playGame (playerInput){
 
     console.log('Wylosowana liczba to: ' + randomNumber);
 
-    /*let computerMove = 'nieznany ruch';
-
-    if(randomNumber == 1){
-        computerMove = 'kamień';
-    } else if (randomNumer == 2) {
-        computerMove = 'papier';
-    } else if (randomNumer == 3) {
-        computerMove = 'nożyce';
-    }*/
-
     let computerMove = getMoveName(randomNumber);
 
     printMessage('Mój ruch to: ' + computerMove);
 
-    /*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
-
     console.log('Gracz wpisał: ' + playerInput);
-
-    /*let playerMove = 'nieznany ruch';
-
-    if(playerInput == '1'){
-    playerMove = 'kamień';
-    } else if (playerInput == '2'){
-        playerMove = 'papier';
-    } else if (playerInput == '3'){
-        playerMove = 'nożyce';
-    }*/
 
     let playerMove = getMoveName(playerInput);
 
